f46103bf895ce733206218d2ac45941f65ae06e7
wanghc00 <306093226@qq.com>
2019-09-02 23:51:44 +0800

    作业提交

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CounterGroup/>
+  </div>
+</template>
+
+<script>
+import CounterGroup from './components/CounterGroup.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CounterGroup
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
